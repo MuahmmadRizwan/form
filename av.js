e5af2a217887321195b9333758a9b88f37d2dc12
MuahmmadRizwan <127449442+MuahmmadRizwan@users.noreply.github.com>
2023-10-28 00:30:29 -0700

    Add files via upload

---
+++ av.js
@@ -0,0 +1,32 @@
+
+    document.getElementById('booking-final').addEventListener('click', function () {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var selectedOption = document.querySelector('select').value;
+        var serviceDate = document.getElementById('service').value;
+        var specialRequest = document.getElementById('Request').value;
+
+        var emailBody = "Name: " + name + "\n" +
+            "Email: " + email + "\n" +
+            "Selected Option: " + selectedOption + "\n" +
+            "Service Date: " + serviceDate + "\n" +
+            "Special Request: " + specialRequest;
+
+        var emailLink = "mailto:m.rizwanhaider11@gmail.com?subject=Booking Request&body=" + encodeURIComponent(emailBody);
+        window.location.href = emailLink;
+    });
+    
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            alert("Geolocation is not supported by your browser.");
+        }
+    }
+
+    function showPosition(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var locationInput = document.getElementById("location");
+        locationInput.value = latitude + ", " + longitude;
+    }
